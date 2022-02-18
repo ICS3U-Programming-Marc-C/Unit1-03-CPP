79915d0eed6d25c494209a8f59b3e8907cafef7b
Ubuntu <ubuntu@ip-172-31-45-190.ec2.internal>
2022-02-18 02:33:51 +0000

    Pushing c++ code to github

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2022 Coffi All rights reserved.
+
+
+// Created by: Marc Coffi
+
+// Date: Feb 17, 2022
+
+// This is a "Hello, World!" program with style.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
